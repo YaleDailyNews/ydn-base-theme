76bb9f21a18bfac9da5db8700c8bb074c3e7d0d2
Soham Sankaran <YDNadmin@client-24.ydn.yale.edu>
2013-11-19 22:03:54 -0500

    Test fix

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 not-found">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Oops! That page cannnot be found.', 'ydn' ); ?></h1>
+					<h1 class="entry-title"><?php _e( 'Oops! That page cannot be found.', 'ydn' ); ?></h1>
 				</header>
 
 				<div class="entry-content">
